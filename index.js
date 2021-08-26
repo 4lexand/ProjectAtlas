8033bf557a0c396e24cc994e5d398d2508650c41
jalex099 <mm18014@ues.edu.sv>
2021-08-26 09:54:19 -0600

    a

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-function Indumentaria() {}
-
-function Camisa(id, stock, nombre, precio) {
+function Indumentaria(id, stock, nombre, precio) {
   this.id = id;
   this.stock = stock;
   this.nombre = nombre;
@@ -8,13 +6,22 @@ function Camisa(id, stock, nombre, precio) {
 }
 
 const tiendaVirtual = (function () {
-  let inventario = [{}, {}, {}];
+  let inventario = [
+    new Indumentaria(0, 2, "Camisa", 10.5),
+    new Indumentaria(1, 2, "Pantalon", 25.0),
+    new Indumentaria(2, 2, "Zapatos", 15.6),
+  ];
   let carritoDeCompras = [];
   let agregarProductos = function (product) {
     this.carritoDeCompras.push(product);
     console.log("El producto ha sido agregado al carrito");
   };
-  let actualizarInventario = function () {};
+
+  let actualizarInventario = function () {
+    return new Promise((resolve, reject) => {});
+  };
   let realizarPago = function () {};
-  return { inventario, carritoDeCompras, agregarProductos };
+  return { inventario, carritoDeCompras, agregarProductos, realizarPago };
 })();
+
+console.log(tiendaVirtual.inventario);
