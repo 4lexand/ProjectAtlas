4729b60a4104f9e86f33f259a7c88e0cd1f2015d
jalex099 <mm18014@ues.edu.sv>
2021-08-26 10:10:45 -0600

    a

--- index.js
+++ index.js
@@ -17,16 +17,30 @@ const tiendaVirtual = (function () {
     console.log("El producto ha sido agregado al carrito");
   };
 
-  let actualizarInventario = function () {
+  let actualizarInventario = function actualizarInventario() {
     return new Promise((resolve) => {
-      for(let i =0; i<this.carritoDeCompras.length; i++){
-        let productSell = this.carritoDeCompras[i]
-        this.inventario[productSell.id].stock - productSell.
+      if (this.carritoDeCompras.length > 0) {
+        for (let i = 0; i < this.carritoDeCompras.length; i++) {
+          let productSell = this.carritoDeCompras[i];
+          this.inventario[productSell.id].stock =
+            this.inventario[productSell.id].stock - productSell.cantidad;
+        }
       }
+      resolve("Inventario Actualizado");
     });
   };
-  let realizarPago = function () {};
+  let realizarPago = async function () {
+    let actualizarInventarioPago = await this.actualizarInventario;
+    console.log(actualizarInventarioPago);
+    return actualizarInventarioPago;
+  };
   return { inventario, carritoDeCompras, agregarProductos, realizarPago };
 })();
 
+console.log(tiendaVirtual.agregarProductos({ id: 0, cantidad: 1 }));
 console.log(tiendaVirtual.inventario);
+console.log(tiendaVirtual.carritoDeCompras);
+
+tiendaVirtual.realizarPago().then((response)=>{
+  console.log(response);
+})
