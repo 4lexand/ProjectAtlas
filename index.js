e112824d0222c77bb72f57bd89d9183714b2f46b
jalex099 <mm18014@ues.edu.sv>
2021-08-26 09:47:57 -0600

    a

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+function Indumentaria() {}
+
+function Camisa(id, stock, nombre, precio) {
+  this.id = id;
+  this.stock = stock;
+  this.nombre = nombre;
+  this.precio = precio;
+}
+
 const tiendaVirtual = (function () {
-  let inventario = [];
+  let inventario = [{}, {}, {}];
   let carritoDeCompras = [];
   let agregarProductos = function (product) {
     this.carritoDeCompras.push(product);
     console.log("El producto ha sido agregado al carrito");
   };
-  let actualizarInventario = function () {
-    
-  }
-  let realizarPago= function () {
-    
-  }
+  let actualizarInventario = function () {};
+  let realizarPago = function () {};
   return { inventario, carritoDeCompras, agregarProductos };
 })();
